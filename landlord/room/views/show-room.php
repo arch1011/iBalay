7edaf121a36a7382ae9bd0c2bd7cddb61c308de2
Arch1011 <cordmorale.arch.101@gmail.com>
2024-05-08 13:15:58 +0800

    modal-room, fetch-roomjs

--- landlord/room/views/show-room.php
+++ landlord/room/views/show-room.php
@@ -47,7 +47,7 @@ $conn->close();
         <div class="card-body">
             <h5 class="card-title">Room List</h5>
 
-            <table class="datatable table table-striped table-bordered">
+            <table class="datatable">
                 <thead>
                     <tr>
                         <th>Room Number</th>
@@ -78,71 +78,12 @@ $conn->close();
         </div>
     </div><!-- End card -->
 
-    <div class="modal fade" id="roomDetailsModal" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Room Details</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body" id="roomDetailsContent">
-                    <!-- Content to be loaded dynamically -->
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    </div><!-- End Room Details Modal -->
+
+                    <?php
+                    include('views/modal-room.php');
+                    ?>
 
 </main><!-- End #main -->
 
 <!-- JavaScript to Populate the Modal -->
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-    const modal = document.getElementById("roomDetailsModal");
-
-    modal.addEventListener("show.bs.modal", function(event) {
-        const button = event.relatedTarget;
-        const roomDetails = JSON.parse(button.getAttribute("data-room-details"));
-
-        const roomNumber = roomDetails.room_number;
-        const landlordId = roomDetails.landlord_id;
-
-        const photo1 = `/iBalay/uploads/roomphotos/room${roomNumber}_landlord${landlordId}/${roomDetails.room_photo1}`;
-        const photo2 = `/iBalay/uploads/roomphotos/room${roomNumber}_landlord${landlordId}/${roomDetails.room_photo2}`;
-
-        const carousel = `
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="${photo1}" class="d-block w-100" alt="Room Photo 1">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="${photo2}" class="d-block w-100" alt="Room Photo 2">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        `;
-
-        const roomInfo = `
-            <h5>Room ${roomNumber}</h5>
-            <p>Description: ${roomDetails.description}</p>
-            <p>Capacity: ${roomDetails.capacity}</p>
-            <p>Price: $${roomDetails.room_price}</p>
-        `;
-
-        document.getElementById("roomDetailsContent").innerHTML = carousel + roomInfo;
-    });
-
-    new simpleDatatables.DataTable(".datatable");
-});
-</script>
+<script src="views/tasks/fetch-room.js"></script>
