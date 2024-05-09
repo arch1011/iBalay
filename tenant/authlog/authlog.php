d1d9658fd1996cdf2606c208939a4f5badc21c8a
Arch1011 <cordmorale.arch.101@gmail.com>
2024-05-09 13:31:37 +0800

    new tenant folder

---
+++ tenant/authlog/authlog.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/img/evsu.png" rel="icon">
+    <link rel="stylesheet" href="css/login.css" />
+</head>
+
+<body>
+
+
+<?php
+  include('layouts/loginauth.php');
+?>
+
+<script src="js/auth.js"></script>
+
+</html> 
