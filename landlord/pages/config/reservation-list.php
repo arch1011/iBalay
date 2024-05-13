9ae445173a6112651075bc1cf7e810b2569a2054
Arch1011 <cordmorale.arch.101@gmail.com>
2024-05-14 02:55:49 +0800

    payment working

--- landlord/pages/config/reservation-list.php
+++ landlord/pages/config/reservation-list.php
@@ -12,7 +12,7 @@ include('../../database/config.php');
 // Turn on error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$query = "SELECT t.FirstName, t.LastName, r.room_number, r.room_price, rr.reserved_id, r.room_photo1, r.room_photo2
+$query = "SELECT t.FirstName, t.LastName, t.TenantID, r.room_id, r.room_number, r.room_price, rr.reserved_id, r.room_photo1, r.room_photo2, r.landlord_id
           FROM tenant t
           INNER JOIN reserved_room rr ON t.TenantID = rr.TenantID
           INNER JOIN room r ON rr.room_id = r.room_id
@@ -58,45 +58,47 @@ if ($result) {
             
             echo '<div class="modal fade" id="roomModal' . $row['room_number'] . '" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title">Room Details</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <h5>Room Number: ' . $row['room_number'] . '</h5>
-                  <div id="carouselExampleControls' . $row['room_number'] . '" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img src="' . $photo1 . '" class="d-block w-100" alt="Room Photo 1">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="' . $photo2 . '" class="d-block w-100" alt="Room Photo 2">
-                      </div>
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Room Details</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <h5 style="margin-top:10px;" >Room Price: $' . $row['room_price'] . '</h5>
-                    <hr>
-                    <h5>Tenant Name: ' . $row['FirstName'] . ' ' . $row['LastName'] . '</h5>
-                    <hr>
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Process</button>
-                    <button type="button" class="btn btn-danger" onclick="declineReservation(' . $row['reserved_id'] . ')" data-bs-dismiss="modal">Decline</button>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls' . $row['room_number'] . '" data-bs-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls' . $row['room_number'] . '" data-bs-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="visually-hidden">Next</span>
-                    </button>
-                  </div>
+                    <div class="modal-body">
+                        <h5>Room Number: ' . $row['room_number'] . '</h5>
+                        <div id="carouselExampleControls' . $row['room_number'] . '" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="' . $photo1 . '" class="d-block w-100" alt="Room Photo 1">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="' . $photo2 . '" class="d-block w-100" alt="Room Photo 2">
+                                </div>
+                            </div>
+                            <h5 style="margin-top:10px;">Room Price: $' . $row['room_price'] . '</h5>
+                            <hr>
+                            <h5>Tenant Name: ' . $row['FirstName'] . ' ' . $row['LastName'] . '</h5>
+                            <hr>
+                            <button type="button" class="btn btn-primary" onclick="storeIDsAndRedirect(' . $row['room_id'] . ', ' . $row['TenantID'] . ', ' . $row['landlord_id'] . ')">Process</button>
+                            <button type="button" class="btn btn-danger" onclick="declineReservation(' . $row['reserved_id'] . ')" data-bs-dismiss="modal">Decline</button>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls' . $row['room_number'] . '" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls' . $row['room_number'] . '" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
                 </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-              </div>
             </div>
-          </div>';
-    
+        </div>';
+        
+        
+         
         }
     } else {
         echo "There is no reservation at the moment.";
@@ -115,3 +117,13 @@ mysqli_close($conn);
             ?>
 
             <script src="config/decline.js"></script>
+
+            <script>
+function storeIDsAndRedirect(roomId, tenantId, landlordId) {
+    // Construct the URL with query parameters
+    var url = "final-stage.php?room_id=" + roomId + "&tenant_id=" + tenantId + "&landlord_id=" + landlordId;
+    // Redirect to the final-stage.php page
+    window.location.href = url;
+}
+
+</script>
--- tenant/includes/nav-top.php
+++ tenant/includes/nav-top.php
@@ -1,3 +1,5 @@
+
+
 <nav class="site-nav">
       <div class="container">
         <div class="menu-bg-wrap">
@@ -7,15 +9,15 @@
             <ul
               class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end"
             >
-              <li class="active"><a href="/iBalay/tenant/public/home.php">Home</a></li>
+              <li class="active"><a href="/iBalay/tenant/public/home.php" style="font-size:20px;">Home</a></li>
               <li class="acive">
-                <a href="/iBalay/tenant/public/properties.php">Properties</a>
+                <a href="/iBalay/tenant/public/properties.php" style="font-size:20px;">Properties</a>
               </li>
-              <li><a href="/iBalay/tenant/public/bookmark.php">Bookmarked Rooms</a></li>
-              <li><a href="/iBalay/tenant/public/reserved.php">Reserved Rooms</a></li>
-              <li><a href="/iBalay/tenant/public/bh-list.php">Boarding Houses</a></li>
-              <li><a href="about.html">About</a></li>
-              <li><a href="contact.html">Contact Us</a></li>
+              <li><a href="/iBalay/tenant/public/bookmark.php" style="font-size:20px;">Bookmarked Rooms</a></li>
+              <li><a href="/iBalay/tenant/public/reserved.php" style="font-size:20px;">Reserved Rooms</a></li>
+              <li><a href="/iBalay/tenant/public/bh-list.php" style="font-size:20px;">Boarding Houses</a></li>
+              <li><a href="about.html" style="font-size:20px;">About</a></li>
+              <li><a href="contact.html" style="font-size:20px;">Contact Us</a></li>
             </ul>
 
             <a
@@ -25,8 +27,12 @@
               data-target="#main-navbar"
             >
               <span></span>
+
+              
             </a>
           </div>
         </div>
       </div>
     </nav>
+
+    
